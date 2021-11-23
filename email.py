a447b2168a18b83e27df6c5a449d85c3f66ed5e7
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2021-11-23 15:58:27 -0500

    model for email created -- #1

---
+++ email.py
@@ -0,0 +1,28 @@
+from tkinter import *
+import sqlite3
+import re
+
+class Model:
+
+    def __init__(self, email) -> None: 
+        self.email = email
+    
+    # Making email attribute read-only
+    @property
+    def email(self):
+        return self.__email
+
+    # setting new values to self.email 'read-only' attribute upon validation
+    @email.setter
+    def email (self, value):
+        pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        
+        if re.fullmatch(pattern, value):
+            self.__email = value
+        else: 
+            raise ValueError(f'Invalid entry: {value}')
+        
+    # Opens and appends to text file
+    def save(self):
+        with open('emails.txt', 'a') as file:
+            file.write(self.email + '\n')
