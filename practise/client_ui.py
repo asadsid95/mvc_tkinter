8fd486dfc9e13b15b0f9fb6693c10362c0f6f796
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2021-11-24 16:16:13 -0500

    toplevel widget class Tk as parent for App() + passing that into View() which inherits from Frame class; still unsure about child-parent relation in this program -- #4

--- practise/client_ui.py
+++ practise/client_ui.py
@@ -1,16 +1,16 @@
 from tkinter import *
 
-class View:
-    def __init__(self) -> None:
-        self.root = Tk()
+class View(Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
 
-        self.title_label = Label(self.root, text="Title")
+        self.title_label = Label(self, text="Title")
         self.title_label.grid(row=0,column=0)
 
-        self.title_entry = Entry(self.root, width=30)
+        self.title_entry = Entry(self, width=30)
         self.title_entry.grid(row=0, column=1)
 
-        self.submit = Button(self.root, text="Submit", command=self.submit)
+        self.submit = Button(self, text="Submit", command=self.submit)
         self.submit.grid(row=1, column =0,columnspan=2)
 
         self.controller = None
@@ -22,9 +22,6 @@ class View:
         # call controller, send user input to controller by using controller's specific method
         self.controller.passing(self.title_entry.get())
 
-    def execute(self):
-        self.root.mainloop()
-
 class Controller:
     def __init__(self,model, view) -> None:
         self.model = model
@@ -32,6 +29,7 @@ class Controller:
 
     def passing(self,title):
         self.model.title = title
+        self.model.work()
 
 class Model:
     def __init__(self, title) -> None:
@@ -40,11 +38,17 @@ class Model:
     def work(self):
         print(self.title)
 
-class App():
-    ui_model = Model("")
-    ui_view = View()
-    ui_controller = Controller(ui_model, ui_view)
+class App(Tk):
+    def __init__(self):
+        super().__init__()
 
-    ui_view.set_controller(ui_controller)
+        self.title("Practise MVC")
 
-    ui_view.execute()
+        ui_model = Model("")
+        ui_view = View(self)
+        ui_view.grid(row=0,column=0)
+        ui_controller = Controller(ui_model, ui_view)
+
+        ui_view.set_controller(ui_controller)
+
+App().mainloop()
