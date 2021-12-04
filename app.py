51a2df279df7bd243a5eb2e36ed767ca5daf75d1
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2021-12-04 16:38:28 -0500

    Attempted to inherit Frame into MainApp() but confusion remains, about argument 'toplevel' being passed into parent's __init__; is it because Frame requires a master? in which case, toplevel is being passed to it rightfully -- #8

--- app.py
+++ app.py
@@ -4,17 +4,23 @@ import view2
 from model import *
 from controller import * 
 
-class MainApp:
+class MainApp(tk.Frame):
     def __init__(self, toplevel): # constructor; This creates object with 1 attribute called 'self.toplevel' therefore I need to pass a value for it when instantiating
+        # tk.Frame.__init__(self,toplevel)
         self.toplevel = toplevel
         self.toplevel.title("This is being called from inside MainApp object. We've received tk.Tk() Widget as attribute")
         
-        self.frame = tk.Frame(self.toplevel,bg='light green',width=75,height=90).grid(row=0,column=0)
-        label_in_Frame = tk.Label(self.frame, text="This is inside Frame, which is inside toplevel widget").grid(row=0,column=0)
+        tk.Label(self.toplevel,text="Inside of Frame that ").grid(row=0,column=0)
+
+        # Instead of creating Frame upon instantiation, I'm going to explore subclassing MainApp with Frame 
+        #  
+        # self.frame = tk.Frame(self.toplevel,bg='light green',width=75,height=90).grid(row=0,column=0)
+        # label_in_Frame = tk.Label(self.frame, text="This is inside Frame, which is inside toplevel widget").grid(row=0,column=0)
 
     pass
 
 if __name__=="__main__":
     root = tk.Tk()
     MainApp(root) # passing toplevel widget into MainApp object. This is required as class's constructor has an attribute (i.e. ). T
+    # print(MainApp.__mro__)
     root.mainloop()
