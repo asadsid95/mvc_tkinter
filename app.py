55eea08215cec3034bf29360ac066036690e8472
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2021-12-04 15:44:24 -0500

    Added Frame widget whose parent is the Toplevel Widget. Inside of frame, a label is created -- #8

--- app.py
+++ app.py
@@ -8,7 +8,10 @@ class MainApp:
     def __init__(self, toplevel): # constructor; This creates object with 1 attribute called 'self.toplevel' therefore I need to pass a value for it when instantiating
         self.toplevel = toplevel
         self.toplevel.title("This is being called from inside MainApp object. We've received tk.Tk() Widget as attribute")
-        pass
+        
+        self.frame = tk.Frame(self.toplevel,bg='light green',width=75,height=90).grid(row=0,column=0)
+        label_in_Frame = tk.Label(self.frame, text="This is inside Frame, which is inside toplevel widget").grid(row=0,column=0)
+
     pass
 
 if __name__=="__main__":
