29d9768bcfb5a352ceb33fa617a903f8a2bde9a7
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2021-12-04 17:28:04 -0500

    found methods to return parent and child names of a widget -- #8

--- app.py
+++ app.py
@@ -6,11 +6,15 @@ from controller import *
 
 class MainApp(tk.Frame):
     def __init__(self, toplevel): # constructor; This creates object with 1 attribute called 'self.toplevel' therefore I need to pass a value for it when instantiating
-        # tk.Frame.__init__(self,toplevel)
+        tk.Frame.__init__(self,toplevel,bg='light green')
         self.toplevel = toplevel
         self.toplevel.title("This is being called from inside MainApp object. We've received tk.Tk() Widget as attribute")
         
-        tk.Label(self.toplevel,text="Inside of Frame that ").grid(row=0,column=0)
+        self.test_label = tk.Label(self.toplevel,text="Inside of Frame that's inside of TopLevel").grid(row=0,column=0)
+        self.toplevel.winfo_parent() 
+        print(self.toplevel.winfo_parent() ) # returns blank as it is top level window
+        print(self.toplevel.winfo_children()) # returns childeren in lowest-to-highest in stacking order [<__main__.MainApp object .!mainapp>, <tkinter.Label object .!label>]
+
 
         # Instead of creating Frame upon instantiation, I'm going to explore subclassing MainApp with Frame 
         #  
